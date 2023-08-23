a7dfdefc5062ecd698ed8c6497e8f2f1369961d5
RuneBiz <31344322+RuneBiz@users.noreply.github.com>
2023-08-23 13:46:06 +0800

    Update sample_test_case.py

--- sample_test_case.py
+++ sample_test_case.py
@@ -7,7 +7,8 @@ import time
 def test_login_fail_with_wrong_credentials():
     website = webdriver.Chrome()
     website.get("https://connectosclockin.hrhub.ph/WebBundy")
-    
+
+    time.sleep(2)
     # add wrong username
     website_login_email = website.find_element(By.NAME, "Username")
     website_login_email.send_keys("rballesta")
