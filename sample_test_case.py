784052cbac22476609a826d96cbaaae8b2face11
RuneBiz <31344322+RuneBiz@users.noreply.github.com>
2023-08-23 14:26:47 +0800

    Update sample_test_case.py

--- sample_test_case.py
+++ sample_test_case.py
@@ -5,20 +5,24 @@ import pytest
 import time
 
 def test_login_fail_with_wrong_credentials():
+
+    MY_USERNAME = os.environ["MY_USERNAME"]
+    MY_PASSWORD = os.environ["MY_PASSWORD"]
+    
     website = webdriver.Chrome()
     website.get("https://connectosclockin.hrhub.ph/WebBundy")
-
+    
     time.sleep(2)
     # add wrong username
-    website_login_email = website.find_element('id', "Username")
-    website_login_email.send_keys(${{ secrets.MY_PASSWORD }})
+    website_login_email = website.find_element(By.ID, "Username")
+    website_login_email.send_keys(MY_USERNAME)
         
     # add wrong password
-    website_login_password = website.find_element('id', "Password")
-    website_login_password.send_keys(${{ secrets.MY_USER_NAME }})
+    website_login_password = website.find_element(By.ID, "Password")
+    website_login_password.send_keys(MY_PASSWORD)
     
     # login with fake credentials
-    website_login_button = website.find_element('id', "showCount")
+    website_login_button = website.find_element(By.ID, "showCount")
     website_login_button.click()
     
     time.sleep(10)
