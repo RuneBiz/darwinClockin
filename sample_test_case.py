d88ec3fbbaca6f50a2fc57fc053337c4d61f398f
RuneBiz <31344322+RuneBiz@users.noreply.github.com>
2023-08-23 13:52:51 +0800

    Update sample_test_case.py

--- sample_test_case.py
+++ sample_test_case.py
@@ -10,15 +10,15 @@ def test_login_fail_with_wrong_credentials():
 
     time.sleep(2)
     # add wrong username
-    website_login_email = website.find_element(By.NAME, "Username")
+    website_login_email = website.find_element('id', "Username")
     website_login_email.send_keys("rballesta")
         
     # add wrong password
-    website_login_password = website.find_element(By.NAME, "Password")
+    website_login_password = website.find_element('id', "Password")
     website_login_password.send_keys("enurFrost**1")
     
     # login with fake credentials
-    website_login_button = website.find_element(By.NAME, "showCount")
+    website_login_button = website.find_element('id', "showCount")
     website_login_button.click()
     
     time.sleep(10)
