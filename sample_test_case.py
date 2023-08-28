9c2c1f3c62395920c17042924eb5f52d09a42c49
RuneBiz <31344322+RuneBiz@users.noreply.github.com>
2023-08-28 17:04:29 +0800

    Update sample_test_case.py

--- sample_test_case.py
+++ sample_test_case.py
@@ -5,7 +5,7 @@ import pytest
 import time
 import os
 
-def test_login_fail_with_wrong_credentials():
+def darwin_login_scheduled():
 
     MY_USERNAME = os.environ["MY_USERNAME"]
     MY_PASSWORD = os.environ["MY_PASSWORD"]
